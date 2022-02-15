ae7b04844d87a625edd9480022157f6bd9594cc8
sourcegilmar <gilmardw@gmail.com>
2022-02-15 17:22:41 -0300

    Enumerados . . .

---
+++ src/enumerados/StatusVenda.java
@@ -0,0 +1,5 @@
+package enumerados;
+
+public enum StatusVenda {
+	INICIANDO, PROCESSANDO, IMPRIMINDO, FINALIZANDO;
+}
---
+++ src/enumerados/TipoPagamento.java
@@ -0,0 +1,5 @@
+package enumerados;
+
+public enum TipoPagamento {
+	DINHEIRO, VISTA, CREDITO, DEBITO, CHEQUE;
+}
